cf3cc9f578bf95fd5594681c8c1cb1e02deba870
krystian-mw <49584311+krystian-mw@users.noreply.github.com>
2020-02-28 14:34:37 +0000

    2.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 const config = {
   entry: [
@@ -65,7 +66,8 @@ const config = {
           {
             from: 'public'
           }
-      ])
+      ]),
+      new Dotenv()
   ],
   devtool: 'cheap-module-eval-source-map'
 }
